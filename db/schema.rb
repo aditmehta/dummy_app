d05ffbdf69ab08c38b15cab69a595d7a7a32759f
Adit Mehta <aditmehta10@gmail.com>
2015-10-13 12:27:08 +0530

    show dropdown to logout when a user is logged in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008082352) do
+ActiveRecord::Schema.define(version: 20151013061128) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20151008082352) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
